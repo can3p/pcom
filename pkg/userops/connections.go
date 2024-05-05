b897d7a63c789a8aac01fd6af3b77bc501939f03
Dmitry Petrov <dpetroff@gmail.com>
2024-05-06 00:51:03 +0200

    control page - render connections

--- pkg/userops/connections.go
+++ pkg/userops/connections.go
@@ -5,7 +5,9 @@ import (
 
 	"github.com/can3p/pcom/pkg/model/core"
 	"github.com/google/uuid"
+	"github.com/samber/lo"
 	"github.com/volatiletech/sqlboiler/v4/boil"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 )
 
 // CreateConnection assumes it's run in transaction
@@ -35,4 +37,57 @@ func CreateConnection(ctx context.Context, db boil.ContextExecutor, user1ID stri
 	}
 
 	return nil
+}
+
+func GetDirectUserIDs(ctx context.Context, db boil.ContextExecutor, userID string) ([]string, error) {
+	connections, err := core.UserConnections(
+		core.UserConnectionWhere.User1ID.EQ(userID),
+	).All(ctx, db)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return lo.Map(connections, func(conn *core.UserConnection, index int) string { return conn.User2ID }), nil
+}
+
+func GetDirectAndIndirectUserIDs(ctx context.Context, db boil.ContextExecutor, userID string) (directUserIDs []string, indirectUserIDs []string, err error) {
+	connections, err := core.UserConnections(
+		core.UserConnectionWhere.User1ID.EQ(userID),
+	).All(ctx, db)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	// we want to explude direct connection as well as the user themselves from indirect connections results
+	// this has to be done, since the connection graph is undirected
+	directUserIDs = lo.Map(connections, func(conn *core.UserConnection, index int) string { return conn.User2ID })
+	toExclude := []string{userID}
+	toExclude = append(toExclude, directUserIDs...)
+
+	type connResult struct {
+		UserID string `boil:"user_id"`
+	}
+
+	var indirectUserIDStruct []*connResult
+
+	// https://www.linkedin.com/pulse/you-dont-need-graph-database-modeling-graphs-trees-viktor-qvarfordt-efzof/
+	err = core.NewQuery(
+		qm.Select("conn2.user2_id as user_id"),
+		qm.From("user_connections as conn1"),
+		qm.LeftOuterJoin("user_connections as conn2 on conn1.user2_id = conn2.user1_id"),
+		qm.Where("conn1.user1_id = ?", userID),
+	).Bind(ctx, db, &indirectUserIDStruct)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	indirectUserIDs = lo.Without(
+		lo.Map(indirectUserIDStruct, func(conn *connResult, index int) string { return conn.UserID }),
+		toExclude...,
+	)
+
+	return directUserIDs, indirectUserIDs, nil
 }
--- pkg/web/func.go
+++ pkg/web/func.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/can3p/pcom/pkg/auth"
 	"github.com/can3p/pcom/pkg/model/core"
-	"github.com/samber/lo"
+	"github.com/can3p/pcom/pkg/userops"
 	"github.com/volatiletech/sqlboiler/v4/boil"
 	"github.com/volatiletech/sqlboiler/v4/queries/qm"
 )
@@ -29,8 +29,30 @@ func Index(c context.Context, db boil.ContextExecutor, userData *auth.UserData) 
 	return getBasePage("Super cool pcom", userData)
 }
 
-func Controls(c context.Context, db boil.ContextExecutor, userData *auth.UserData) *BasePage {
-	return getBasePage("Controls", userData)
+type ControlsPage struct {
+	*BasePage
+	DirectConnections   core.UserSlice
+	IndirectConnections core.UserSlice
+}
+
+func Controls(ctx context.Context, db boil.ContextExecutor, userData *auth.UserData) *ControlsPage {
+	directUserIDs, indirectUserIDs, err := userops.GetDirectAndIndirectUserIDs(ctx, db, userData.DBUser.ID)
+
+	// @TODO: all panics should be eliminated later
+	if err != nil {
+		panic(err)
+	}
+
+	directUsers := core.Users(core.UserWhere.ID.IN(directUserIDs)).AllP(ctx, db)
+	indirectUsers := core.Users(core.UserWhere.ID.IN(indirectUserIDs)).AllP(ctx, db)
+
+	controlsPage := &ControlsPage{
+		BasePage:            getBasePage("Controls", userData),
+		DirectConnections:   directUsers,
+		IndirectConnections: indirectUsers,
+	}
+
+	return controlsPage
 }
 
 func Write(c context.Context, db boil.ContextExecutor, userData *auth.UserData) *BasePage {
@@ -141,20 +163,16 @@ func DirectFeed(ctx context.Context, db boil.ContextExecutor, userData *auth.Use
 	user := userData.DBUser
 	title := fmt.Sprintf("%s - Direct Feed", user.Username)
 
-	connections := core.UserConnections(
-		core.UserConnectionWhere.User1ID.EQ(user.ID),
-	).AllP(ctx, db)
+	directUserIDs, err := userops.GetDirectUserIDs(ctx, db, user.ID)
 
-	userIDs := []string{}
-
-	for _, conn := range connections {
-		userIDs = append(userIDs, conn.User2ID)
+	if err != nil {
+		panic(err)
 	}
 
 	directFeedPage := &FeedPage{
 		BasePage: getBasePage(title, userData),
 		Posts: core.Posts(
-			core.PostWhere.UserID.IN(userIDs),
+			core.PostWhere.UserID.IN(directUserIDs),
 			qm.Load(core.PostRels.User),
 			qm.OrderBy(fmt.Sprintf("%s DESC", core.PostColumns.ID)),
 		).AllP(ctx, db),
@@ -169,33 +187,11 @@ func ExploreFeed(ctx context.Context, db boil.ContextExecutor, userData *auth.Us
 	user := userData.DBUser
 	title := fmt.Sprintf("%s - Explore Feed", user.Username)
 
-	connections := core.UserConnections(
-		core.UserConnectionWhere.User1ID.EQ(user.ID),
-	).AllP(ctx, db)
+	_, indirectUserIDs, err := userops.GetDirectAndIndirectUserIDs(ctx, db, user.ID)
 
-	// we want to explude direct connection as well as the user themselves from indirect connections results
-	// this has to be done, since the connection graph is undirected
-	directUserIDs := lo.Map(connections, func(conn *core.UserConnection, index int) string { return conn.User2ID })
-	directUserIDs = append(directUserIDs, user.ID)
-
-	type userID struct {
-		UserID string `boil:"user_id"`
+	if err != nil {
+		panic(err)
 	}
-
-	var indirectUserIDStruct []*userID
-
-	// https://www.linkedin.com/pulse/you-dont-need-graph-database-modeling-graphs-trees-viktor-qvarfordt-efzof/
-	core.NewQuery(
-		qm.Select("conn2.user2_id as user_id"),
-		qm.From("user_connections as conn1"),
-		qm.LeftOuterJoin("user_connections as conn2 on conn1.user2_id = conn2.user1_id"),
-		qm.Where("conn1.user1_id = ?", user.ID),
-	).BindP(ctx, db, &indirectUserIDStruct)
-
-	indirectUserIDs := lo.Without(
-		lo.Map(indirectUserIDStruct, func(conn *userID, index int) string { return conn.UserID }),
-		directUserIDs...,
-	)
 
 	exploreFeedPage := &FeedPage{
 		BasePage: getBasePage(title, userData),
